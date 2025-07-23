e4624696f7547c579a590401442a5b51cfb579e9
Jesse <107804427+Maggarducci@users.noreply.github.com>
2025-07-23 09:13:11 -0500

    Update r2fi.rb

--- r2fi.rb
+++ r2fi.rb
@@ -1,3 +1,4 @@
+# using gets for input as well as passing to_i to celsius string to convert to integer for calculations
 print "Hello. Enter a temp in celsius: "
 celsius = gets
 fahrenheit = (celsius.to_i * 9 / 5) + 32
