c4ad24799f472569d0dc095c4a6ce5d2ef34b204
Jesse <107804427+Maggarducci@users.noreply.github.com>
2025-07-26 15:11:57 -0500

    Add files via upload
    using input from pre-existing data file and printing to newly created output text file
    providing user with strings to include them in the operation and inform them of the location of the new data

---
+++ c2fINOUT.rb
@@ -0,0 +1,12 @@
+puts "reading from celsius file"
+num_in = File.read("temp.dat") # contains 100 as value
+celsius = num_in.to_i
+fahrenheit = (celsius * 9 / 5) + 32
+ 
+# write fahrenheit value to new file
+
+fh = File.new("temp.out", "w")
+fh.puts fahrenheit
+fh.close
+
+puts "fahrenheit value of #{fahrenheit} written to file temp.out"
